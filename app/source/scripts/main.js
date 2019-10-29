1c9a0fdf5f78b8529752f44a2a84903a5a829922
Eugene <eugene.kotsarev@gmail.com>
2019-10-29 03:25:20 +0300

    update 4. mobile menu finally fix mobile menu add new script and refactored previous in ES6 clean up menu.sass

--- app/source/scripts/main.js
+++ app/source/scripts/main.js
@@ -1,103 +1,71 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
 	// AOS init
 	AOS.init({
 		offset: 50
 	});
 
 
-
 	// Меню
-	// body смещается вниз и влево, фон меняется, тени появляются, есть кнопка меню закрыть, при нажатии на бади - закрывается меню
-	// меню не исчезает, оно просто смещено
-	// при нажатии на кнопку происходит смещение
 
 	const pageMobile = document.querySelector('.page');
-	const wrapperMobile = document.querySelector('.wrapper');
 	const innerMobile = document.querySelector(".inner");
 	const headerMobile = document.querySelector('.header');
-	
+	const mobileMenu = document.querySelector('.menu-mobile');
+	const menuMobile = document.querySelector('.nav-mobile');
+	// кнопки
+	const buttonMenu = document.querySelector(".nav__button");
+	const buttonClose = document.querySelector('.nav-mobile__button');
 
-	// const navMenu = document.querySelector('.nav__menu');
-	const navMenuMobile = document.querySelector('.nav-mobile');
-	const navMobileWrapper = document.querySelector('.nav-mobile__wrapper');
+	// переключатели
+	const toggleInner = () => {
+		innerMobile.classList.toggle('inner--mobile');
+	};
+	const togglePage = () => {
+		pageMobile.classList.toggle('page--mobile');
+	};
+	const toggleMobileHeader = () => {
+		headerMobile.classList.toggle('header--mobile');
+	};
+	const toggleMobileNav = () => {
+		menuMobile.classList.toggle('nav-mobile--show');
+	};
 
-	const navButton = document.querySelector(".nav__button");
-	const navButtonMobile = document.querySelector('.nav-mobile__button');
+	// нажатие
+	buttonMenu.addEventListener("click", e => {
+		e.preventDefault(e);
+		e.stopPropagation();
 
-	
-	// On click
-	navButton.addEventListener("click", function (evt) {
-		evt.preventDefault(evt);
-		if (navButton.classList.contains('nav__button--close')) {
+		toggleInner();
+		togglePage();
+		toggleMobileHeader();
+		toggleMobileNav();
+	});
 
-			innerMobile.classList.remove('inner--show');
-			innerMobile.classList.add('inner--static');
-
-			navButton.classList.remove('nav__button--close');
-			// navMenu.classList.remove('nav__menu--show');
-
-			pageMobile.classList.remove('page--mobile');
-			wrapperMobile.classList.remove('wrapper--mobile');
-			headerMobile.classList.remove('header--mobile');
-			navMenuMobile.classList.remove('nav-mobile--show');
-			navMenuMobile.classList.add('visually-hidden');
-			navMobileWrapper.classList.remove('nav-mobile__wrapper--show');
-
-		} else {
-			innerMobile.classList.add('inner--show');
-			innerMobile.classList.remove('inner--static');
-			navButton.classList.add('nav__button--close');
-			// navMenu.classList.add('nav__menu--show');
-			pageMobile.classList.add('page--mobile');
-			wrapperMobile.classList.add('wrapper--mobile');
-			headerMobile.classList.add('header--mobile');
-			navMenuMobile.classList.add('nav-mobile--show');
-			navMenuMobile.classList.remove('visually-hidden');
-			navMobileWrapper.classList.add('nav-mobile__wrapper--show');
-		}
+	buttonClose.addEventListener("click", e => {
+		e.preventDefault(e);
+		e.stopPropagation();
 		
-		// if (navMenu.classList.contains('nav__menu--show')) {
-		// 	navMenu.classList.remove('nav__menu--show');
-		// 	navButton.classList.remove('nav__button--close');
-		// 	navButtonText.textContent = "menu";
-		// 	pageBody.classList.remove('page--show');
-		// } else {
-		// 	navMenu.classList.add('nav__menu--show');
-		// 	navButton.classList.add('nav__button--close');
-		// 	navButtonText.textContent = "close";
-		// 	pageBody.classList.add('page--show');
-		// }
+		toggleInner();
+		togglePage();
+		toggleMobileHeader();
+		toggleMobileNav();
 	});
 
-	
-	navButtonMobile.addEventListener("click", function (evt) {
-		evt.preventDefault(evt);
-		if (navButton.classList.contains('nav__button--close')) {
-			innerMobile.classList.remove('inner--show');
-			innerMobile.classList.add('inner--static');
+	// при клике на пустую область
+	document.addEventListener('click', e => {
+		let target = e.target;
+		let its_menu = target == mobileMenu || mobileMenu.contains(target);
+		let its_buttonClose = target == buttonClose;
+		let its_buttonMenu = target == buttonMenu;
+		let menu_is_active = innerMobile.classList.contains('inner--mobile');
 
-			navButton.classList.remove('nav__button--close');
-			// navMenu.classList.remove('nav__menu--show');
-
-			pageMobile.classList.remove('page--mobile');
-			wrapperMobile.classList.remove('wrapper--mobile');
-			headerMobile.classList.remove('header--mobile');
-			navMenuMobile.classList.remove('nav-mobile--show');
-			navMobileWrapper.classList.remove('nav-mobile__wrapper--show');
-		} else {
-			innerMobile.classList.add('inner--show');
-			innerMobile.classList.remove('inner--static');
-			navButton.classList.add('nav__button--close');
-			// navMenu.classList.add('nav__menu--show');
-			pageMobile.classList.add('page--mobile');
-			wrapperMobile.classList.add('wrapper--mobile');
-			headerMobile.classList.add('header--mobile');
-			navMenuMobile.classList.add('nav-mobile--show');
-			navMobileWrapper.classList.add('nav-mobile__wrapper--show');
+		if (!its_menu && !its_buttonMenu && !its_buttonClose && menu_is_active) {
+			toggleInner();
+			togglePage();
+			toggleMobileHeader();
+			toggleMobileNav();
 		}
 	});
-
-
 
 	// Custom JS
 	console.log('Hello, World!');

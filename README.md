6a3e6e56321da487d39f7450dfeb8af9cf3f1351
Eugene <eugene.kotsarev@gmail.com>
2019-11-06 01:51:45 +0300

    update 7. new base add new folders structure and gulp scripts; lgmflfkgmdfkgm

--- README.md
+++
@@ -1,8 +0,0 @@
-# Portfolio
-
-Work in progress, stay tuned!
-
-Include normalize.css, Lazy loading and AOS.js
-
-## Current state here
-[Click me!](https://ereburg.github.io/portfolio/app/dist)
--- app/gulp/config/tasks.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-    './app/gulp/tasks/pug',
-    './app/gulp/tasks/styles',
-    './app/gulp/tasks/scripts',
-    './app/gulp/tasks/images',
-    './app/gulp/tasks/server'
-];
